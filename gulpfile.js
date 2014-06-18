033c964d202efd5be1baf99d829465f0ee9ac282
softdev520 <softdev520@outlook.com>
2014-06-17 23:47:44 -0400

    Added some npm packages. Created livereload and haml gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,27 @@ var destJsDir  = destDir + '/javascripts'
 var destCssDir = destDir + '/stylesheets'
 
 
+// Live Reload
+// ---------------------------------------------------------
+
+var livereload = require('gulp-livereload');
+
+
+// Haml Tasks
+// ---------------------------------------------------------
+
+var haml = require('gulp-haml');
+
+gulp.task('haml', function() {
+  gulp.src(srcDir + '/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest(destDir))
+});
+
+
 // Coffee Tasks
 // ---------------------------------------------------------
 
-// include plugins
 var coffee = require('gulp-coffee');
 
 gulp.task('coffee', function() {
@@ -32,29 +49,31 @@ gulp.task('coffee', function() {
 // Production Tasks
 // ---------------------------------------------------------
 
-// include plugins
 var concat     = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify     = require('gulp-uglify');
 
-// JS concat, strip debugging and minify
 gulp.task('scripts', function() {
   gulp.src([ srcJsDir + '/lib.js', srcJsDir + '/*.js'])
     .pipe(concat('script.js'))
     .pipe(stripDebug())
     .pipe(uglify())
-    .pipe(gulp.dest(destJsDir + '/'));
+    .pipe(gulp.dest(destJsDir + '/'))
+    .pipe(livereload());
 });
 
 
 // Default Gulp Task
 // ---------------------------------------------------------
 
-gulp.task('default', ['coffee', 'scripts'], function () {
+gulp.task('default', ['haml', 'coffee', 'scripts'], function () {
 
   // watch the coffeescript dir
   gulp.watch(srcCoffeeDir + '/*.coffee', ['coffee']);
 
-  // watch the coffeescript dir
+  // watch the js dir
   gulp.watch(srcJsDir + '/*.js', ['scripts']);
+
+  // watch the haml files
+  gulp.watch(srcDir + '/*.haml', ['haml']);
 });
